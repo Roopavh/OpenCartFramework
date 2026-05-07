c85f4663d7e80a3e13ebf60eaf5549af2f2447bc
Roopa  VH <roopad@Riskcovry-Roopa-D.local>
2026-05-07 14:21:34 +0530

    Implemented the logger in allure listener class

--- src/test/java/com/qa/opencart/base/BaseTest.java
+++ src/test/java/com/qa/opencart/base/BaseTest.java
@@ -63,6 +63,6 @@ public class BaseTest {
 	@AfterTest
 	public void tearDown() {
 		driver.quit();
-		log.info("-------Closing browser-------");
+		log.info("-------Closing the browser-------");
 	}
 }
